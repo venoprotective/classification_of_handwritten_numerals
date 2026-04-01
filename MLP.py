dfd71bb8432c70f409dfad4e6afc6eff9a4e27cb
venoprotective <venoprotective@gmail.com>
2026-04-01 12:50:57 +0300

    split data

--- MLP.py
+++ MLP.py
@@ -1,25 +1,15 @@
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+
 
 X, y = fetch_openml('mnist_784', version=1, 
                     return_X_y=True)
 
-X = X.values
-y = y.astype(int).values
+X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=10_000,
+                                                  random_state=123, stratify=y)
 
-# print(X.shape)
-# print(y.shape) 
+X_train, X_valid, y_train, y_valid = train_test_split(X_temp, y_temp, test_size=5000,
+                                                      random_state=123, stratify=y_temp)
 
-
-fig, ax = plt.subplots(nrows=2, ncols=5,
-                       sharex=True, sharey=True)
-ax = ax.flatten()
-for i in range(10):
-    img = X[y == i][0].reshape(28, 28)
-    ax[i].imshow(img, cmap='Greys')
-    
-ax[0].set_xticks([])
-ax[0].set_yticks([])
-plt.tight_layout()
-plt.show()
