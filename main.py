64c46821f87266ee46ee630be132b031ee176c68
venoprotective <venoprotective@gmail.com>
2026-04-01 12:55:39 +0300

    sigmoid & onehot

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+def int_to_onehot(y, num_labels):
+    
+    ary = np.zeros((y.shape[0], num_labels))
+    for i, val in enumerate(y):
+        ary[i, val] = 1
+    
+    return ary
 
 X, y = fetch_openml('mnist_784', version=1, 
                     return_X_y=True)
